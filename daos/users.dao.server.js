1f85276d5692972925bdbab058aa2640e4e1a45f
shruthi-raghuraman <raghuraman.s@husky.neu.edu>
2020-04-20 23:29:55 -0400

    first commit

---
+++ daos/users.dao.server.js
@@ -0,0 +1,35 @@
+const userModel = require('../models/users.model.server')
+
+const createUser = (user) => {
+    return userModel.create(user)
+}
+const findAllUsers = () => {
+    return userModel.find()
+}
+const findUserById = (userId) => {
+    return userModel.findOne(
+        {
+            _id: userId
+        }
+    )
+}
+
+const findUserByCredentials =
+    (username, password) => {
+        return userModel.findOne(
+            {
+                username: username,
+                password :password
+            })
+    }
+
+const deleteUser = (uid) =>
+    userModel.deleteOne({_id: uid})
+
+module.exports = {
+    createUser,
+    findAllUsers,
+    findUserById,
+    findUserByCredentials,
+    deleteUser
+}
