046b9e64b77254973312275e4204bae72b0ce0b9
ffeghali <feghali.fa@husky.neu.edu>
2020-04-23 03:15:40 -0400

    minor edits to styling

--- client/src/components/PrivacyPolicy.js
+++ client/src/components/PrivacyPolicy.js
@@ -3,9 +3,10 @@ import './PrivacyPolicy.css'
 
 const PrivacyPolicy = () => {
     return (
-        <div>
+        <div className='container mt-4 mb-4'>
+            <h1 className='display-5 app-header-font'>Privacy Policy</h1>
             <a href='https://docs.google.com/document/d/1AX_2OyUw_O9tF5MsVrMtBrt3kCTIJTpYo8lNcSZpBow/edit?usp=sharing'>
-                Click this link to view our privacy policy document.
+                Please click this link to view our privacy policy document.
             </a>
         </div>
     )
--- client/src/components/users/Profile.js
+++ client/src/components/users/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { profile, updateSubscribers } from "../../services/userService";
+import "../../common/style.css";
 
 export default class Profile extends React.Component {
   state = {
@@ -28,45 +29,45 @@ export default class Profile extends React.Component {
     const urlTest = "hello there";
     const listItems = this.state.profile.subscribeToOthers;
     return (
-      <div>
-        <h1>Profile</h1>
+      <div className='container mt-4 mb-4'>
+        <h1 className='display-3 app-header-font'>Profile</h1>
         console.log({urlTest});
         <p>{this.state.profile.first}</p>
         <p>{this.state.profile.last}</p>
         <p>{JSON.stringify(this.state.profile)}</p>
         <button
           onClick={() => this.props.history.push("./update-profile")}
-          className={"btn btn-primary btn-block"}
+          className='btn app-primary-button app-margin-block col-5'
         >
           Update Profile Information
         </button>
         <button
           onClick={() => this.props.history.push("./view-all-users")}
-          className={"btn btn-primary btn-block"}
+          className='btn app-primary-button app-margin-block col-5'
         >
           View All Users
         </button>
         <button
           onClick={() => this.props.history.push("./delete-user")}
-          className={"btn btn-primary btn-block"}
+          className='btn app-primary-button app-margin-block col-5'
         >
           Delete User
         </button>
         <button
           onClick={() => this.props.history.push("./subscribe-to-others")}
-          className={"btn btn-primary btn-block"}
+          className='btn app-primary-button app-margin-block col-5'
         >
           Subscribe to Other Users
         </button>
         <button
           onClick={() => this.props.history.push("./")}
-          className={"btn btn-primary btn-block"}
+          className='btn app-primary-button app-margin-block col-5'
         >
           Home
         </button>
-        <h5>Cooks I follow!</h5>
+        <h5 className= 'display-5 app-header-font'>Cooks I follow!</h5>
         <p>{listItems}</p>
-        <h5>Cooks that follow me!</h5>
+        <h5 className= 'display-5 app-header-font'>Cooks that follow me!</h5>
         <p>{this.state.profile.subscriptionsFromOthers}</p>
       </div>
     );
