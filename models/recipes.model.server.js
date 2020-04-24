0d929211509e55bbc7e6f6155c5c062a5d022a3f
shruthi-raghuraman <raghuraman.s@husky.neu.edu>
2020-04-23 21:41:22 -0400

    recipes repeat removed

--- models/recipes.model.server.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const recipeSchema = require('./recipes.schema.server')
-const recipeModel = mongoose.model(
-    'RecipeModel',
-    recipeSchema
-)
-module.exports = recipeModel
